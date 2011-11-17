2e367241fe72c4945b6dfb1c7cf09649961b4cc9
Mark Cowley <m.cowley0@gmail.com>
2011-11-17 09:32:18 +0000

    update to roxygen2

--- R/updateR-package.R
+++ R/updateR-package.R
@@ -1,7 +1,7 @@
 #' Package development toolkit.
 #' A collection of functions that enable R package maintainers to build and update packages. 
 #' 
-#' \code{\link{updateR}} runs \code{roxygen}, \code{R CMD CHECK}, \code{R CMD BUILD} and 
+#' \code{\link{updateR}} runs \code{roxygen2}, \code{R CMD CHECK}, \code{R CMD BUILD} and 
 #' \code{R CMD INSTALL} to generate polished R packages. Execution stops upon errors, and 
 #' all messages are hidden unless there are errors.
 #' 
@@ -31,4 +31,4 @@
 #' @seealso \code{\link{updateR}}, \code{\link{mjc2roxygen}}, \code{\link[updateR]{which.package}}, \code{\link{get.function.dependencies}}
 #' @keywords package
 #'
-roxygen()
+NULL
