ef65f9664787a85bf8f13d6dd639355913f072b3
Mark Cowley <m.cowley0@gmail.com>
2011-12-01 03:01:53 +0000

    major roxupdate; RDa's merged into one.

--- R/edit.func.R
+++ R/edit.func.R
@@ -1,14 +1,36 @@
+#' edit an R source file
+#' 
 #' Search for the .R file that contains a function, and open it in an editor.
+#' It searches for the file containing the function definition, by grepping
+#' for the pattern \code{"^my.func.name <- function"}, where \code{my.func.name}
+#' is the name of func.
 #'
-#' @param func a function name, or the function's code
+#' There are functions which work with textmate on OSX (textmate, tm, mate; 
+#' smultron), or on Linux (kate = KDE Advanced Text Editor).
+#' 
+#' Common usage scenarios are that you have a folder full of R packages under
+#' development. In this case, set \code{src.root}, or better yet, the
+#' \code{options(src.root="/path/to/source/root")} to point to the top level
+#' folder containing all the R packages. 
+#' If you also have a directory full of .R files, then you can provide these
+#' files via the \code{src.files} argument.
+#' 
+#' @param func the quoted function name, or the function's code
 #' @param src.root The parent folder of source code
 #' @param src.files an optional vector of filenames to search within
-#' @param editor the path to your editor.
-#' @param exclude.patterns vector of patterns passed to grep for files to exclude
+#' @param editor the path to your editor. Default = \code{getOption("editor")}
+#' @param exclude.patterns vector of patterns passed to grep for files to exclude.
 #' @return nothing. The side effect is that an R source file is opened in an editor.
 #' @author Mark Cowley, 26/2/07
 #' @export
-edit.src.file <- function(func, src.root=getOption("src.root"), src.files=NULL, editor=NULL, exclude.patterns=c("~", ".Rcheck", ".git", ".svn")) {
+#' @rdname edit.src.file
+#' @seealso \code{\link{find.src.file}}
+#' @examples
+#' \dontrun{
+#' edit.src.file("edit.src.file", "~/src/R")
+#' edit.src.file("edit.src.file", "~/src/R", editor="open -a TextMate")
+#' }
+edit.src.file <- function(func, src.root=getOption("src.root"), src.files=NULL, editor=getOption("editor"), exclude.patterns=c("~", ".Rcheck", ".git", ".svn")) {
 	if ( !is.character(func) )
 		func <- as.character(substitute(func))
 	if( is.null(editor) )
@@ -26,53 +48,50 @@ edit.src.file <- function(func, src.root=getOption("src.root"), src.files=NULL, 
 		cat(sprintf("goto line: %d, then call:  updateR(%s)\n", lineno, p))
 	}
 }
+# CHANGELOG
+# 2011-11-24:
+# - major roxygen merge of all functions in this file.
+# - bug fix each wrapper function to substitute the value of func before
+#   calling edit.src.file
+# 
 
-#' Edit source file in kate (KDE advanced text editor)
-#' @param func a function name, or the function's code
-#' @param src.root The parent folder of source code
-#' @return nothing. The side effect is that an R source file is opened in an editor.
-#' @author Mark Cowley, 26/2/07
+
+#' @rdname edit.src.file
 #' @export
 kate <- function(func, src.root=getOption("src.root")) {
+	if ( !is.character(func) )
+		func <- as.character(substitute(func))
 	edit.src.file(func=func, src.root=src.root, editor="/usr/bin/kate -u")
 }
 
-#' Edit source file in TextMate
-#' @param func a function name, or the function's code
-#' @param src.root The parent folder of source code
-#' @return nothing. The side effect is that an R source file is opened in an editor.
-#' @author Mark Cowley, 26/2/07
+#' @rdname edit.src.file
 #' @export
 textmate <- function(func, src.root=getOption("src.root")) {
+	if ( !is.character(func) )
+		func <- as.character(substitute(func))
 	edit.src.file(func=func, src.root=src.root, editor="open -a TextMate")
 }
 
-#' Edit source file in TextMate
-#' @param func a function name, or the function's code
-#' @param src.root The parent folder of source code
-#' @return nothing. The side effect is that an R source file is opened in an editor.
-#' @author Mark Cowley, 26/2/07
+#' @rdname edit.src.file
 #' @export
 tm <- function(func, src.root=getOption("src.root")) {
+	if ( !is.character(func) )
+		func <- as.character(substitute(func))
 	textmate(func, src.root)
 }
 
-#' Edit source file in TextMate
-#' @param func a function name, or the function's code
-#' @param src.root The parent folder of source code
-#' @return nothing. The side effect is that an R source file is opened in an editor.
-#' @author Mark Cowley, 26/2/07
+#' @rdname edit.src.file
 #' @export
 mate <- function(func, src.root=getOption("src.root")) {
+	if ( !is.character(func) )
+		func <- as.character(substitute(func))
 	textmate(func, src.root)
 }
 
-#' Edit source file in Smultron
-#' @param func a function name, or the function's code
-#' @param src.root The parent folder of source code
-#' @return nothing. The side effect is that an R source file is opened in an editor.
-#' @author Mark Cowley, 26/2/07
+#' @rdname edit.src.file
 #' @export
 smultron <- function(func, src.root=getOption("src.root")) {
+	if ( !is.character(func) )
+		func <- as.character(substitute(func))
 	edit.src.file(func=func, src.root=src.root, editor="open -a Smultron")
 }
