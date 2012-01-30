cbf8f6686f3c20e65ce405fed5b186d499d71613
Mark Cowley <m.cowley0@gmail.com>
2012-01-30 00:17:36 +0000

    - simplified & using detach - This still doesn't get around the dreaded error:   "Error in fetch(key) : internal error -3 in R_decompress1" - According to Ripley's reply, it's impossible to do so:   (http://r.789695.n4.nabble.com/Unable-to-reload-Rdoc-td4333063.html)   "This is simply not supported.  Lazy-load databases are cached, and you   cannot expect to change them during the R session once they have been used."

--- R/unlibrary.R
+++ R/unlibrary.R
@@ -1,14 +1,12 @@
 #' Unload a library.
+#' 
 #' Unload a library which has already been \code{load}ed. Adapted from \code{\link{relibrary}}
 #' @param package Name or character string giving the name of a package.
 #' @param character.only A logical indicating whether \sQuote{package} can be
-#'   assumed to be character strings. Default value is \code{FALSE}.
-#' @param warn.conflicts If \code{TRUE}, warnings are printed about conflicts
-#'   from reattaching of the package, unless that package contains an object
-#'   \code{.conflicts.OK}. Default value is \code{FALSE}.
+#'     assumed to be character strings. Default value is \code{FALSE}.
 #' @param \dots Currently ignored.
 #' @author Mark Cowley, based on \code{\link{relibrary}} from Henrik Bengtsson, \email{henrikb@@braju.com},
-#'   \url{http://www.braju.com/R/}
+#'     \url{http://www.braju.com/R/}
 #' @seealso See \code{\link[base]{library}} \code{\link{relibrary}}.
 #' @return nothing.
 #' @examples
@@ -17,29 +15,33 @@
 #' }
 #' @export
 #' 
-unlibrary <- function(package, character.only=FALSE, warn.conflicts=TRUE, ...) {
-  if (!character.only)
-    package <- as.character(substitute(package));
+unlibrary <- function(package, character.only=FALSE, ...) {
+	if (!character.only) {
+		package <- as.character(substitute(package))
+		character.only <- TRUE
+	}
 
-  # If package is already attached, then detach it first.
-  pkgName <- paste(sep="", "package:", package);
-  pos <- match(pkgName, search());
-  if (!is.na(pos)) {
-    # If there exists a function .Last.lib() in the package call it first!
-    if (exists(".Last.lib", where=pos, inherits=FALSE)) {
-      .Last.lib <- get(".Last.lib", pos=pos, inherits=FALSE);
-      if (is.function(.Last.lib)) {
-        libpath <- attr(pos.to.env(pos), "path");
-        if (!is.null(libpath))
-          try(.Last.lib(libpath));
-      }
-    }
-    # ...then remove the package.
-    .Internal(detach(pos));
-  }
+	if (paste("package:",package,sep="") %in% search()){
+		detach(pos=match(paste("package:",package,sep=""), search()), unload=TRUE, force=TRUE)
+	}
+	
+	# if( package %in% loadedNamespaces() ) {
+	# 	unloadNamespace(package)
+	# }
+	# else if (paste("package:",package,sep="") %in% search()){
+	# 	detach(pos=match(paste("package:",package,sep=""), search()), unload=TRUE, force=TRUE)
+	# }
 }
 ############################################################################
 # HISTORY:
+# 2012-01-24
+# - simplified & using detach
+# - This still doesn't get around the dreaded error: 
+#   "Error in fetch(key) : internal error -3 in R_decompress1"
+# - According to Ripley's reply, it's impossible to do so:
+#   (http://r.789695.n4.nabble.com/Unable-to-reload-Rdoc-td4333063.html)
+#   "This is simply not supported.  Lazy-load databases are cached, and you
+#   cannot expect to change them during the R session once they have been used."
 # 2001-08-06
 # * Now relibrary will set the option "relibrary" to the name of the package
 # it is currently reloading. This option can be used by the package itself
