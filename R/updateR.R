35c6652ed318a934b1c36c49add9ff9b2f37928c
Mark Cowley <m.cowley0@gmail.com>
2012-04-05 00:56:36 +0000

    added -d flag for DEPLOY to enzo. tidied code added @importFrom improved documentation bug fix in @TODO section using \describe{…} rather  than \begin{itemize}…\end{itemize}

--- R/updateR.R
+++ R/updateR.R
@@ -1,17 +1,20 @@
 #' A flexible R package updater.
 #' 
-#' This function can be used to roxygenize, check, build source/binary/windows binary, install
-#' and reload R packages. 
+#' This function can be used to roxygenize, check, build source/binary/windows binary, install,
+#' reload, test and deploy R packages. 
 #' Under-the-hood, it calls a shell script, which in turn calls various \dQuote{R CMD}
-#' programs, like R CMD CHECK, R CMD roxygenize, R CMD BUILD, R CMD INSTALL.
+#' programs, like R CMD {CHECK,BUILDINSTALL}, in addition to some others, including
+#' roxygen, testthat.
 #' This function assumes that you develop your R packages within a top level directory, eg ~/src/R
-#' given by the \dQuote{src.root} option.
+#' given by the \code{options("src.root")} option.
 #' 
 #' I tend to edit my source code in an external editor (TextMate), then will want to incorporate
 #' those code changes into my current R session. A simple updateR("my.package", source=TRUE, install=TRUE),
-#' causes the package to be built and then installed. Optional roxygenizing, package checking, 
-#' can occur, prior to package building, and 
-#' package testing (via \code{testthat}) after installing
+#' causes the package to be built and then installed. 
+#' Optional roxygenizing & package path checking can be run prior to package building;\cr
+#' source or binary package bundle building;\cr
+#' installation, reloading the package in the current session;\cr
+#' followed by testing via Hadley Wickham's \code{testthat} and deployment to enzo after installation.
 #'
 #' @section Roxygen2:
 #' roxygen2 is an inline documentation engine which builds \code{Rd} files from structured comment 
@@ -25,6 +28,21 @@
 #' roxygen comments. Try the \code{\link[Rd2roxygen]{Rd2roxygen}} package for converting from 
 #' \code{Rd} to \code{roxygen} comments.
 #'
+#' @section Checking:
+#' \code{R CMD CHECK} is run on the package folder, not the package bundle. It's on my todo list to
+#' supporte \code{CHECK}ing of the tar.gz, possibly with the \code{--as-cran} option.
+#' 
+#' @section Testing:
+#' If the package contains a testthat package suite, then selecting \code{test=TRUE} will
+#' run a \code{\link[testthat]{test_package}} on the \code{package}.
+#' 
+#' @section Deploy:
+#' if \code{deploy=TRUE}, then the newly built package bundle will be deployed, ie copied and
+#'  installed to enzo. Note that if you run updateR with \code{source=FALSE, binary=FALSE},
+#'  then the existing package bundle will be deployed. This assumes that you have setup
+#'  password-less authentication on the target host, and the username is the same as on the
+#'  submission machine.
+#' 
 #' @note
 #' Currently, \code{\link{relibrary}} is unable to update the R documentation objects, giving 
 #' internal errors.
@@ -36,9 +54,12 @@
 #' }
 #'
 #' @section TODO:
-#' \begin{itemize}
+#' \describe{
 #'   \item{devtools}{Investigate more of the \code{devtools} functions for build/check/install.}
-#' \end{itemize}
+#'   \item{Checking bundle}{Run R CMD CHECK --as-cran on the package bundle. Why? 'cos that's what\cr
+#' CRAN runs their checks on -- not the folder which may mask errors due to the way i've setup my
+#' environment.}
+#' }
 #' 
 #' @param package the name of the package to be updated. either quoted, or unquoted.
 #' @param src.root the path to the root of the src files.
@@ -54,12 +75,20 @@
 #' @param test logical: run a \code{testthat::\link[testthat]{test_package}} suite, 
 #'   on the installed version of the package? Note this is done after this function
 #'   is given the opportunity to install the package.
+#' @param deploy logical: if \code{TRUE}, then deploy the \code{package_VERSION.tar.gz} to\cr
+#'   enzo. Note if you don't build a new package (ie \code{source=FALSE}, and \code{binary=FALSE}),
+#'   then this will deploy the existing version of the package bundle, else the newly
+#'   created package bundle will be deployed.
 #' @param no.vignettes logical: if \dQuote{TRUE}, turn off the creation of vignettes
 #' @param no.manual logical: if \dQuote{TRUE}, turn off the creation of PDF manuals
-#' @param no.docs if TRUE, logical: if \dQuote{TRUE}, turn off the creation of documentation
+#' @param no.docs logical: if \dQuote{TRUE}, turn off the creation of documentation
+#' 
 #' @export
+#' @importFrom devtools reload
+#' 
 #' @seealso \code{\link{relibrary}}, \code{\link[roxygen2]{roxygenize}}, \code{\link{.Rprofile}}, \code{\link{install.packages}}
 #' @author Mark Cowley
+#' 
 #' @examples 
 #' \dontrun{
 #' # build a source package, and install.
@@ -67,6 +96,8 @@
 #' updateR("updateR", "~/src/R", source=TRUE, install=TRUE)
 #' # roxygenize, check, and then build a source package, and install.
 #' updateR("updateR", "~/src/R", roxygen=TRUE, check=TRUE, source=TRUE, install=TRUE)
+#' # roxygenize, check, and then build a source package, and install + test & deploy.
+#' updateR("updateR", "~/src/R", roxygen=TRUE, check=TRUE, source=TRUE, install=TRUE, test=TRUE, deploy=TRUE)
 #' }
 updateR <- function(package, src.root=getOption("src.root"), lib.loc=NULL, warn.conflicts = TRUE, 
 	roxygen=FALSE,
@@ -76,7 +107,11 @@ updateR <- function(package, src.root=getOption("src.root"), lib.loc=NULL, warn.
 	winbinary=FALSE, 
 	install=TRUE,
 	test=FALSE,
+	deploy=FALSE,
 	no.vignettes=FALSE, no.manual=FALSE, no.docs=FALSE, no.examples=FALSE) {
+
+	################################################################################
+	# check args
 	if ( !is.character(package) ) 
 		package <- as.character(substitute(package))
 	
@@ -87,6 +122,16 @@ updateR <- function(package, src.root=getOption("src.root"), lib.loc=NULL, warn.
 	}
 	src.root <- path.expand(src.root)
 
+	if( is.null(lib.loc) )
+		lib.loc <- .libPaths()[1]
+	if( !file.exists(lib.loc) ) {
+		stop("lib.loc must be specified. Currently it's: ", shQuote(lib.loc))
+	}
+	lib.loc <- path.expand(lib.loc)
+	################################################################################
+	
+	################################################################################
+	# get the package path & check it's structure
 	package.path <- file.path(src.root, package)
 	if( !file.exists(package.path) ) {
 		stop("Source code could not be found at", shQuote(package.path))
@@ -94,20 +139,14 @@ updateR <- function(package, src.root=getOption("src.root"), lib.loc=NULL, warn.
 	if( !all(file.exists(file.path(package.path, c("R", "DESCRIPTION")))) ) {
 		stop("package does not contain an R folder, and/or DESCRIPTION file", shQuote(package.path))
 	}
+	################################################################################
 	
-	if( is.null(lib.loc) )
-		lib.loc <- .libPaths()[1]
-	if( !file.exists(lib.loc) ) {
-		stop("lib.loc must be specified. Currently it's: ", shQuote(lib.loc))
-	}
-	lib.loc <- path.expand(lib.loc)
-	
+	################################################################################
+	# setup the arguments for running updateR.sh
 	exe <- file.path(.path.package('updateR'), 'bin', 'updateR.sh')
-	if( !file.exists(exe) ) {
-		stop("updateR.sh could not be found at", shQuote(exe))
-	}
+	file.exists(exe) || stop("updateR.sh could not be found at", shQuote(exe))
 	
-	options <- sprintf("-l %s %s %s %s %s %s %s %s %s %s %s %s", 
+	options <- sprintf("-l %s %s %s %s %s %s %s %s %s %s %s %s %s", 
 						lib.loc, 
 						ifelse(roxygen,      "-r", ""), 
 						ifelse(check,        "-c", ""), 
@@ -116,10 +155,11 @@ updateR <- function(package, src.root=getOption("src.root"), lib.loc=NULL, warn.
 						ifelse(winbinary,    "-w", ""),
 						ifelse(no.vignettes, "-g", ""),
 						ifelse(no.manual,    "-m", ""),
-						ifelse(no.docs,      "-d", ""),
+						ifelse(no.docs,      "-o", ""),
 						ifelse(no.examples,  "-x", ""),
 						ifelse(install,      "-i", ""),
-						ifelse(test,         "-t", "")
+						ifelse(test,         "-t", ""),
+						ifelse(deploy,       "-d", "")
 						)
 	options <- trim(gsub(" +", " ", options))
 	cmd <- sprintf('%s %s %s > /dev/stderr; echo $?', 
@@ -128,17 +168,23 @@ updateR <- function(package, src.root=getOption("src.root"), lib.loc=NULL, warn.
 		package.path
 	) # the redirect allows $? to be stored in retval below.
 	# cat(cmd, "\n")
-
+	################################################################################
+	
+	################################################################################
+	# run the script
 	retval <- system(cmd, intern=TRUE)
 	if( length(retval) == 0 )
 		stop("updateR.sh command failed with no output.\n")
 	retval <- as.numeric(retval)
-
+	################################################################################
+	
+	################################################################################
+	# reload the package in the current session
 	if( install ) {
 		if( retval == 0 ) {
 			cat("Reloading", package, "\n")
 			# relibrary(package, character.only=TRUE, warn.conflicts=warn.conflicts)
-			devtools::reload(package)
+			reload(package)
 		}
 		else {
 			cat("Library build failed. not reloaded.\n")
@@ -154,7 +200,9 @@ updateR <- function(package, src.root=getOption("src.root"), lib.loc=NULL, warn.
 # 2012-03-16: added a -t flag for running testthat::test_package
 #             update the Date field in the DESCRIPTION file every time rox/build is run
 #             uses devtools::reload to reload the package
-
+# 2012-04-05: added -d flag for DEPLOY to enzo.
+# 			  tidied code
+#             added @importFrom
 
 # # Update a package which is contained within a package bundle (ie a meta.package).
 # #
